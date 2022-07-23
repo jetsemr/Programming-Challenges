863af844b7ee419f44b3107383e3354d6130792a
jet.semrick@gmail.com <jet.semrick@gmail.com>
2022-07-23 13:40:25 -0400

    Day 3 Success

---
+++ 3. 7-22-2022/join-lists.py
@@ -0,0 +1,11 @@
+# 7-22-2022
+# Write a function that concatenates two lists in alternating ways.
+
+def joinListsAlts(arr1, arr2):
+  array = []
+  for n in arr1:
+    array.append(n)
+    array.append(arr2[arr1.index(n)])
+  return array
+
+print (joinListsAlts([1,2,3],[4,5,6]))
