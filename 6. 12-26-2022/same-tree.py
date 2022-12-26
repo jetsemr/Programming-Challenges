94eb5c37676378ed4da6ff53eb4d294613fee7ea
jet.semrick@gmail.com <jet.semrick@gmail.com>
2022-12-26 16:04:01 -0600

    same tree analyzer

---
+++ 6. 12-26-2022/same-tree.py
@@ -0,0 +1,26 @@
+# Author: Jet Semrick
+# Date: 12-21-2022
+# Problem: Given the roots of two binary trees p and q, 
+# write a function to check if they are the same or not.
+
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if p == None and q == None:
+            return True
+        if p == None or q == None:
+            return False
+        if p.val != q.val:
+            return False
+        
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
