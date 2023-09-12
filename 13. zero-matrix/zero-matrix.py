bd9d4158aba8891a790d52044ac8e80dd01859c7
jet.semrick@gmail.com <jet.semrick@gmail.com>
2023-09-12 15:39:57 +0800

    Zero Matrix

---
+++ 13. zero-matrix/zero-matrix.py
@@ -0,0 +1,25 @@
+# Author: Jet Semrick
+# Date: 09-12-2023
+# Problem: Implement a algorithm to rotate a matrix 90 degrees.
+# Source: 1.8 from pg. 91 Cracking the Coding Interview
+
+def zero(arr):
+  cols = {}
+  rows = {}
+  for i in range(len(arr)):
+    for j in range(len(arr[i])):
+      if j not in rows:
+        if arr[i][j] == 0:
+          rows[j] = True
+          cols[i] = True
+
+  for i in range(len(arr)):
+    for j in range(len(arr[i])):
+      if i in cols or j in rows:
+        arr[i][j] = 0
+
+  return arr
+
+# Test Cases
+arr = [[0,2,3,4],[1,2,3,4],[1,2,0,4]]
+print(zero(arr) == [[0, 0, 0, 0], [0, 2, 0, 4], [0, 0, 0, 0]])
