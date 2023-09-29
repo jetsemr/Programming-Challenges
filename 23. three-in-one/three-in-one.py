d3a95263e83281ae08a265542065e9815a4797b3
jet.semrick@gmail.com <jet.semrick@gmail.com>
2023-09-29 16:56:08 +0800

    Three Stack Array

---
+++ 23. three-in-one/three-in-one.py
@@ -0,0 +1,30 @@
+class MultiStack:
+  def __init__(self, stacksize):
+    self.numstacks = 3
+    self.array = [0] * (stacksize * self.numstacks)
+    self.sizes = [0] * self.numstacks
+    self.stacksize = stacksize
+
+  def push(self, item, stacknum):
+    if self.isFull(stacknum):
+      raise Exception('Full!')
+    self.sizes[stacknum] += 1
+    self.array[self.indexOfTop(stacknum)] = item
+
+  def pop(self, stacknum):
+    if self.isEmpty(stacknum):
+      raise Exception('Empty!')
+    value = self.array[self.indexOfTop(stacknum)] = 0
+    self.array[self.indexOfTop(stacknum)] = 0
+    self.sizes[stacknum] -= 1
+    return value
+
+  def isEmpty(self, stacknum):
+    return self.sizes[stacknum] == 0
+
+  def isFull(self, stacknum):
+    return self.sizes[stacknum] == self.stacksize
+
+  def indexOfTop(self, stacknum):
+    offset = stacknum * self.stacksize
+    return offset + self.sizes[stacknum] - 1
