8e197ba42df591343d90db06e36a64e81f09a90e
jet.semrick@gmail.com <jet.semrick@gmail.com>
2023-10-17 22:19:48 +0800

    Stack Module

---
+++ 4. Stacks & Queues/sort-stack.py
@@ -0,0 +1,7 @@
+# Author: Jet Semrick
+# Date: 10-17-2023
+# Problem: Write a program to sort a stack such that the smallest items are on top. You can use an additional temporary stack, but you may not copy the elements into any other data structure.
+# Source: 3.5 from pg. 99 Cracking the Coding Interview
+
+def sort_stack():
+  return 1
