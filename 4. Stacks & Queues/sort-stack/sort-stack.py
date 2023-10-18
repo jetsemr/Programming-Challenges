b1b2a09dd75e0aa1310497d6636ec2ef1e43b5a7
jet.semrick@gmail.com <jet.semrick@gmail.com>
2023-10-18 14:12:31 +0800

    Min Stack

---
+++ 4. Stacks & Queues/sort-stack/sort-stack.py
@@ -0,0 +1,48 @@
+# Author: Jet Semrick
+# Date: 10-18-2023
+# Problem: Produce a stack that is ordered with min at the top. You can use another stack.
+# Source: 3.5 from pg. 99 Cracking the Coding Interview
+
+import sys
+sys.path.append("..")
+import stack
+
+class SortedStack:
+  def __init__(self):
+    self.main = stack.Stack()
+    self.helper = stack.Stack()
+  
+  def push(self, value):
+    while (not self.main.isEmpty() and self.main.peek() < value):
+       self.helper.push(self.main.pop())
+    self.main.push(value)
+    while (not self.helper.isEmpty()):
+      self.main.push(self.helper.pop())
+  
+  def pop(self):
+    return self.main.pop()
+  
+  def peek(self):
+    return self.main.peek()
+
+  def isEmpty(self):
+    return self.main.isEmpty()
+    
+
+# Test Cases
+
+s = SortedStack()
+s.push(1)
+s.push(2)
+s.push(3)
+print(s.peek() == 1)
+s.push(7)
+s.push(8)
+s.push(4)
+s.pop()
+print(s.peek() == 2)
+s.push(5)
+s.push(6)
+print(s.peek() == 2)
+s.pop()
+print(s.peek() == 3)
