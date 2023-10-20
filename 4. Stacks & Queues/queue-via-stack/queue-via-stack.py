6232d34f68850f5434218226e8533eb5ac159b92
jet.semrick@gmail.com <jet.semrick@gmail.com>
2023-10-20 12:59:07 +0800

    Stack of Plates

--- 4. Stacks & Queues/queue-via-stack/queue-via-stack.py
+++ 4. Stacks & Queues/queue-via-stack/queue-via-stack.py
@@ -3,42 +3,17 @@
 # Problem: Produce a queue using two stacks.
 # Source: 3.4 from pg. 99 Cracking the Coding Interview
 
-class Node:
-  def __init__(self, value):
-    self.value = value
-    self.next = None
-
-class Stack:
-  def __init__(self):
-    self.head = Node("head")
-    self.size = 0
-  def getSize(self):
-    return self.size
-  def isEmpty(self):
-    return self.size == 0
-  def peek(self):
-    if self.isEmpty():
-      raise Exception("Peeking empty stack")
-    return self.head.next.value
-  def push(self, value):
-    node = Node(value)
-    node.next = self.head.next
-    self.head.next = node
-    self.size += 1
-  def pop(self):
-    if self.isEmpty():
-      raise Exception("Popping from empty stack")
-    remove = self.head.next
-    self.head.next = self.head.next.next
-    self.size -= 1
-    return remove.value
+import sys
+sys.path.append("..")
+# pylint: disable=import-error
+import structures
 
 # Queue has FIFO order
 class Queue:
   def __init__(self):
     self.size = 0
-    self.old = Stack()
-    self.new = Stack()
+    self.old = structures.Stack()
+    self.new = structures.Stack()
 
   def add(self, value):
     self.new.push(value)
--- 4. Stacks & Queues/stack-of-plates/stack-of-plates.py
+++ 4. Stacks & Queues/stack-of-plates/stack-of-plates.py
@@ -3,30 +3,49 @@
 # Problem: There is a max stack height. If exceeded, create a new stack. This should operate like a single stack. 
 # Source: 3.3 from pg. 99 Cracking the Coding Interview
 
-class Stack:
+import sys
+sys.path.append("..")
+# pylint: disable=import-error
+import structures
 
-  def __init__(self, size):
-    self.size = size
-  def push(self, item):
-    return -1
+class PlateStack:
+  def __init__(self, maxSize):
+    self.arr = [structures.Stack()]
+    self.maxSize = maxSize
+    self.size = 0
+
+  def push(self, value):
+    currIndex = len(self.arr) - 1
+    if self.arr[currIndex].size == self.maxSize:
+      self.arr.append(structures.Stack())
+      currIndex += 1
+    self.arr[currIndex].push(value)
+    self.size += 1
+
   def pop(self):
-    return -1
+    currIndex = len(self.arr) - 1
+    val = self.arr[currIndex].pop()
+    self.size -= 1
+    if currIndex != 0 and self.arr[currIndex].isEmpty():
+      self.arr.pop()
+    return val
+  
+  def peek(self):
+    currIndex = len(self.arr) - 1
+    return self.arr[currIndex].peek()
+  
+  def isEmpty(self):
+    return self.arr[0].isEmpty()
+  
+  def getSize(self):
+    return self.size
 
-stacks = []
-capacity = 10
-def push(item):
-  # curr should get last stack
-  curr = None
-  if not curr.isFull():
-    curr.push()
-  else:
-    stack = Stack(capacity)
-    stack.push(item)
-    stacks.append(stack)
+# Test Cases
+s = PlateStack(2)
+print(s.isEmpty())
+for i in range(10):
+  s.push(i)
 
-def pop():
-  # last should get last stack
-  last = None
-  last.pop()
-  if last.size == 0:
-    stacks.pop(len(stacks) - 1)
+print(s.peek() == 9)
+print(s.pop() == 9)
+print(s.getSize() == 9)
